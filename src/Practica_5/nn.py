c07090f70bdbcf657ec19f8ffda081c5e42a17cc
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2024-03-26 18:35:11 +0100

    Practica_5-> Inicio

---
+++ src/Practica_5/nn.py
@@ -0,0 +1,105 @@
+import numpy as np
+import scipy.io as sio
+import utils
+import matplotlib.pyplot as plt
+
+
+def cost(theta1, theta2, X, y, lambda_):
+    """
+    Compute cost for 2-layer neural network. 
+
+    Parameters
+    ----------
+    theta1 : array_like
+        Weights for the first layer in the neural network.
+        It has shape (2nd hidden layer size x input size + 1)
+
+    theta2: array_like
+        Weights for the second layer in the neural network. 
+        It has shape (output layer size x 2nd hidden layer size + 1)
+
+    X : array_like
+        The inputs having shape (number of examples x number of dimensions).
+
+    y : array_like
+        1-hot encoding of labels for the input, having shape 
+        (number of examples x number of labels).
+
+    lambda_ : float
+        The regularization parameter. 
+
+    Returns
+    -------
+    J : float
+        The computed value for the cost function. 
+
+    """
+
+    return J
+
+
+def backprop(theta1, theta2, X, y, lambda_):
+    """
+    Compute cost and gradient for 2-layer neural network. 
+
+    Parameters
+    ----------
+    theta1 : array_like
+        Weights for the first layer in the neural network.
+        It has shape (2nd hidden layer size x input size + 1)
+
+    theta2: array_like
+        Weights for the second layer in the neural network. 
+        It has shape (output layer size x 2nd hidden layer size + 1)
+
+    X : array_like
+        The inputs having shape (number of examples x number of dimensions).
+
+    y : array_like
+        1-hot encoding of labels for the input, having shape 
+        (number of examples x number of labels).
+
+    lambda_ : float
+        The regularization parameter. 
+
+    Returns
+    -------
+    J : float
+        The computed value for the cost function. 
+
+    grad1 : array_like
+        Gradient of the cost function with respect to weights
+        for the first layer in the neural network, theta1.
+        It has shape (2nd hidden layer size x input size + 1)
+
+    grad2 : array_like
+        Gradient of the cost function with respect to weights
+        for the second layer in the neural network, theta2.
+        It has shape (output layer size x 2nd hidden layer size + 1)
+
+    """
+
+    return (J, grad1, grad2)
+
+
+def loadData():
+    data = sio.loadmat('data/ex3data1.mat', squeeze_me=True)
+    X = data['X']
+    y = data['y']
+    return X, y
+
+
+def displayData(X):
+    rand_indices = np.random.choice(X.shape[0], 100, replace=False)
+    utils.displayData(X[rand_indices, :])
+    plt.savefig('memoria/images/dataset.png')
+
+
+def main():
+    X, y = loadData()
+    print("X,y loaded")
+    displayData(X)
+
+
+if __name__ == '__main__':
+    main()
