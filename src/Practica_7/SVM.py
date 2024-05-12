ef1419ba2ead29f054d38374fe7391cf3eca89e9
Alejandro Barrachina Argudo <44205191+ALK222@users.noreply.github.com>
2024-05-12 18:10:29 +0200

    encoding

--- src/Practica_7/SVM.py
+++ src/Practica_7/SVM.py
@@ -372,12 +372,10 @@ def Pruebas():
     input_layer_size = X.shape[1]
     hidden_layer_size = 125
     num_labels = 2
-    yA = [0 if i == 1 else 1 for i in y_train]
-    yB = [1 if i == 1 else 0 for i in y_train]
-    y_encoded = np.array([yA, yB]).T
+
     theta1 = np.random.rand(hidden_layer_size, input_layer_size + 1)
     theta2 = np.random.rand(num_labels, hidden_layer_size + 1)
-    _, _, _, theta1, theta2, nn_trainer.train_model(X_train, y_encoded, theta1,
+    _, _, _, theta1, theta2, nn_trainer.train_model(X_train, y_train, theta1,
                                                     theta2, best_params[0], best_params[1], 1000)
     yA = [0 if i == 1 else 1 for i in y_test]
     yB = [1 if i == 1 else 0 for i in y_test]
